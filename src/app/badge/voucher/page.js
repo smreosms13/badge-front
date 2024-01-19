d0214ee3cecf22da03f62cbde3513fefb61ff9ed
smreosms13 <smreosms13@gamil.com>
2024-01-20 00:53:06 +0900

    feat: make dummy pages for route path - /badge - /badge/certificate - /badge/voucher

---
+++ src/app/badge/voucher/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <main className="mx-auto max-w-sm h-dvh grid grid-col-3 gap-3 shadow-2xl px-6 py-12 bg-white">
+            <p>badge-voucher</p>
+        </main>
+    );
+}
