b49db6917b59b5d33495e1baf1402fbef405d4e8
smreosms13 <smreosms13@gamil.com>
2024-01-20 00:42:29 +0900

    chore: install tailwindcss and configure

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,13 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily:{
+        inter: ["var(--inter)"],
+        poppins : ["var(--poppins)"]
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require("tailwind-scrollbar-hide")
+  ],
 }
