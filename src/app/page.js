b9d89cac0aab3b27ecec8224f7e925337cbdaf26
smreosms13 <smreosms13@gamil.com>
2024-01-20 01:05:57 +0900

    feat: adjust BadgeContainer, BtnNav, ListBox into main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,15 @@
-export default function Home() {
+import BadgeContainer from "./ui/BadgeContainer";
+import BtnNav from "./ui/BtnNav";
+import ListBox from "./ui/ListBox";
 
+export default function Home() {
+  const BadgeContainerName = '디지털 증명서'
+  const ListBoxName = '디지털 바우처'
   return (
     <main className="mx-auto max-w-sm h-dvh grid grid-col-3 gap-3 shadow-2xl p-6 bg-white">
-      <div>NextJS</div>
+      <BadgeContainer name={BadgeContainerName}></BadgeContainer>
+      <BtnNav></BtnNav>
+      <ListBox name={ListBoxName}></ListBox>
     </main>
   );
 }
