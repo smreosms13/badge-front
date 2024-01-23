800b48a2f205ae771c29a5779d41f4ba7d2385de
smreosms13 <smreosms13@gamil.com>
2024-01-24 00:31:25 +0900

    #8 comment:Todo - put values of content in page(/certificate/info)

--- src/app/badge/certificate/info/page.js
+++ src/app/badge/certificate/info/page.js
@@ -1,9 +1,9 @@
 import Banner from "@/app/ui/certificate/Banner";
 import Table from "@/app/ui/certificate/Table";
 export default function Page() {
-    const BtnName = "새로운 배지 만들기"
     return (
         <>
+            {/* TODO - component에 props로 content 주기 */}
             <Banner></Banner>
             <Table></Table>
         </>
