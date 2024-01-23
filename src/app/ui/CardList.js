71a9e2dfa128f2169c3fd1a461c07f65999e8b75
smreosms13 <smreosms13@gamil.com>
2024-01-23 14:53:02 +0900

    design : fix style of CardList

--- src/app/ui/CardList.js
+++ src/app/ui/CardList.js
@@ -2,7 +2,7 @@ import Card from "./Card"
 export default function CardList({name, contents, CustomCard}) {
     const CardComponent = CustomCard ? CustomCard : Card;
     return(
-        <div className="flex flex-col overflow-scroll scrollbar-hide">
+        <div className="flex flex-col overflow-y-scroll scrollbar-hide">
             {name && (
                 <div className="my-3 font-medium text-lg">
                     <p>{name}</p>
