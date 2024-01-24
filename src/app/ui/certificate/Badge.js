fa989d54e494cd49010d217ccad36b14cfc9ae7d
smreosms13 <smreosms13@gamil.com>
2024-01-24 15:56:50 +0900

    feat: regardless of the value of isValid, show all badges

--- src/app/ui/certificate/Badge.js
+++ src/app/ui/certificate/Badge.js
@@ -4,15 +4,16 @@ import {
 } from '@heroicons/react/24/solid';
 
 export default function Badge({content}) {
-    const isVerified = JSON.parse(content.isValid)
+    const isValid = JSON.parse(content.isValid);
+    const isVerified = JSON.parse(content.isVerified);
 
     return(
         <Link
             href={`/badge/certificate/${content.tokenId}/detail`}
             className="flex flex-col items-center "
         >
-            <div className="flex justify-center items-center relative w-20 h-20 rounded-xl border">
-                <img src={content.image} className="w-14 h-14 bg-blue-900 fill-white"></img>
+            <div className={`flex justify-center items-center relative w-20 h-20 rounded-xl border ${!isValid ? 'bg-gray-500' : ''}`}>
+                <img src={content.image} className={`w-14 h-14 ${!isValid? 'brightness-[50%]':''}`}></img>
                 {isVerified && (
                     <CheckBadgeIcon className="absolute -right-3 -top-1 w-6 h-6 fill-yellow-400"></CheckBadgeIcon>
                 )}
