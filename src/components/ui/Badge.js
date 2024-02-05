309f395272d093b0e4538def43659e8b77856d8e
smreosms13 <smreosms13@gamil.com>
2024-02-05 16:06:54 +0900

    design : set Icon as alternative to image of Badge

--- src/components/ui/Badge.js
+++ src/components/ui/Badge.js
@@ -3,17 +3,22 @@ import Image from "next/image";
 import {
     CheckBadgeIcon
 } from '@heroicons/react/24/solid';
+import { IdentificationIcon } from "@heroicons/react/24/outline";
 
 export default function Badge({content}) {
-    const isValid = JSON.parse(content.isValid)
-    const isVerified = JSON.parse(content.isVerified)
+    const isValid = JSON.parse(content?.isValid)
+    const isVerified = JSON.parse(content?.isVerified)
     return(
         <Link
-            href={`/badge/certificate/${content.id}/detail`}
+            href={`/badge/certificate/${content?.id}/detail`}
             className="flex flex-col p-1 items-center"
         >
             <div className="flex justify-center items-center relative  rounded-xl">
-                <Image src={content.image} alt={content.badgeName} width={30} height={35} className={`${!isValid? 'brightness-[70%]':''}`}></Image>
+                {content.image !== "" ? (
+                        <Image src={content?.image} alt={content.badgeName} width={30} height={35} className={`${!isValid? 'brightness-[70%]':''}`}/>) 
+                    : (
+                        <IdentificationIcon className={`w-9 h-9 fill-white ${!isValid? 'brightness-[70%]':''}`}></IdentificationIcon>
+                    )}
                 {isVerified && (
                     <CheckBadgeIcon className="absolute -right-3 -top-1 w-4 h-4 fill-yellow-400"></CheckBadgeIcon>
                 )}
