a5c7e197ea488bfc0245664bf233effabdc1d4e8
smreosms13 <smreosms13@gamil.com>
2024-02-20 13:46:20 +0900

    #21 feat : check if connected user wallet has badge minted

--- src/components/ui/Badge.js
+++ src/components/ui/Badge.js
@@ -4,10 +4,42 @@ import {
     CheckBadgeIcon
 } from '@heroicons/react/24/solid';
 import { IdentificationIcon } from "@heroicons/react/24/outline";
+import { useAccount, useBalance, useContractRead  } from 'wagmi';
+import { NftAddress } from '@/contractInfo/address';
+import { Abi } from '@/contractInfo/abi';
+import { useAuth } from "@/context/Context";
+import { useState } from "react";
+
 
 export default function Badge({content}) {
+    const [isMintedNFT, setIsMintedNFT] = useState(false);
     const isValid = JSON.parse(content?.isValid)
     const isVerified = JSON.parse(content?.isVerified)
+    const account = useAccount();
+
+    if(account?.isConnected && isVerified){
+        if (content?.tokenId.length !== 0 ) {
+            const uintTokenId = BigInt(content?.tokenId)
+        
+            //ignore problems 
+            //React Hook "useContractRead" is called conditionally. React Hooks must be called in the exact same order in every component render.
+            const contractRead = useContractRead({
+                abi: Abi,
+                address: NftAddress,
+                functionName: 'ownerOf',
+                args: [uintTokenId],
+                onSuccess(data) {
+                    console.log('Success', data);
+                    console.log('Wallet Address', account?.address)
+
+                  },
+            });
+            if(contractRead?.data === account?.address) {
+                setIsMintedNFT(true);
+            } 
+        }
+    }
+    
     return(
         <Link
             href={`/badge/certificate/${content?.id}/detail`}
@@ -19,7 +51,7 @@ export default function Badge({content}) {
                     : (
                         <IdentificationIcon className={`w-9 h-9 fill-white ${!isValid? 'brightness-[70%]':''}`}></IdentificationIcon>
                     )}
-                {isVerified && (
+                {isMintedNFT && (
                     <CheckBadgeIcon className="absolute -right-3 -top-1 w-4 h-4 fill-yellow-400"></CheckBadgeIcon>
                 )}
             </div>
---
+++ src/contractInfo/address.js
@@ -0,0 +1,1 @@
+export const NftAddress = "0x28D5d5310659C50aF29363e3b2a6b555E48e64f4"
