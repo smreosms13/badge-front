438315739cf04a28596d80090e204af266f4565e
smreosms13 <smreosms13@gamil.com>
2024-02-20 13:48:53 +0900

    fix : check if contents is empty string

--- src/components/homepage/main/BadgeContainer.js
+++ src/components/homepage/main/BadgeContainer.js
@@ -6,8 +6,6 @@ import { useAuth } from '@/context/Context';
 import { redirect, useRouter } from 'next/navigation';
 import axios from 'axios';
 
-
-
 export default function BadgeContainer({name}) {
     const [isLoading, setIsLoading] = useState(true);
     const { currentUser } = useAuth();
@@ -16,24 +14,18 @@ export default function BadgeContainer({name}) {
     
     useEffect(() => {
         fetchData();
-        return(()=>{
-            fetchData()
-        })
     }, []);
 
     const fetchData = async () => {
         try {
-
-            console.log(currentUser.uid)
+            console.log(currentUser?.uid)
             const dataToSend = {
                 userId : currentUser?.uid
             }
-
             const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/getAllMyVDBs`, dataToSend );
             const data = response.data
             setContents(data);
             setIsLoading(false);
-          
         } catch (error) {
           console.error('Error fetching data:', error);
         }
@@ -42,7 +34,6 @@ export default function BadgeContainer({name}) {
     if(!currentUser){
         router.push('/')
     } else {
-
         return(
             <>
                 <div className="flex flex-col rounded-3xl h-48 bg-blue-950 p-2">
@@ -52,7 +43,7 @@ export default function BadgeContainer({name}) {
                     {isLoading ?  (
                             <p className='text-white text-lg text-center'>Loading....</p>
                         ) : (
-                        contents ? (
+                        (contents.length !== 0 ) ? (
                             <CarouselContainer contents={contents} CarouselComponent={Badge} />
                         )  : (
                             <p className='text-white text-lg text-center'>No Data....</p>
