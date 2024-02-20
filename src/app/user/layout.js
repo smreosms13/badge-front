e436e0f28469227e4455af6b0bcf3af70932894f
smreosms13 <smreosms13@gamil.com>
2024-02-20 15:18:46 +0900

    feat : add layout at user pages and apply auth-route

---
+++ src/app/user/layout.js
@@ -0,0 +1,24 @@
+"use client"
+
+import Header from "@/components/ui/Header";
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/context/Context';
+
+export default function Layout({children}) {
+    const { currentUser } = useAuth();
+    const router = useRouter();
+
+    if(!currentUser){
+        router.push('/')
+    }
+    else{
+        return (
+            <div className="mx-auto max-w-sm h-dvh flex flex-col shadow-2xl px-6 py-8 bg-white">
+                <Header></Header>
+                <div className="w-full grow overflow-y-scroll scrollbar-hide">
+                    {children}
+                </div>
+            </div>
+        );
+    }
+}
