385d2363f3384852fd4d0a298764c0c31e90a618
smreosms13 <smreosms13@gamil.com>
2024-02-20 21:35:35 +0900

    #21 feat: check if wallet connected and control connectModal

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,19 +1,30 @@
 "use client"
-
-
 import BadgeContainer from "@/components/homepage/main/BadgeContainer";
 import BtnNav from "@/components/homepage/main/BtnNav";
 import VoucherList from "@/components/homepage/main/VoucherList";
 import Header from "@/components/homepage/main/Header";
 import { useAuth } from "@/context/Context";
 import { useRouter } from "next/navigation";
+import { useAccount } from "wagmi";
+import { useConnectModal} from '@rainbow-me/rainbowkit';
+import { useState, useEffect } from "react";
 
 export default function Page() {
 
   const BadgeContainerName = '디지털 증명서';
   const { currentUser } = useAuth();
   const router = useRouter();
+  const account = useAccount();
+  const { openConnectModal } = useConnectModal();
+  const [modalOpened, setModalOpened] = useState()
 
+  useEffect(() => {
+    if (currentUser && account?.isDisconnected && !modalOpened) {
+      openConnectModal();
+      setModalOpened(true);
+    }
+  }, [currentUser, account, modalOpened, openConnectModal]);
+  
   if(!currentUser){
     router.push('/')
   } else {
