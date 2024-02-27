9b29df3d4cfdf98f9d54a48a7308fb2c7cbf1aee
smreosms13 <smreosms13@gamil.com>
2024-02-27 16:26:02 +0900

    fix: fix typo in user guide props

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -60,7 +60,7 @@ export default function Page() {
         <VoucherList />
 
         {!guideOpened} && (
-          <Dialog open={!guideOpened} onOpenChange={setGuideOpened}>
+          <Dialog open={guideOpened} onOpenChange={setGuideOpened}>
             <DialogContent className="max-w-sm">
                 <DialogHeader className="items-center">
                     <div className='mb-3'></div>
