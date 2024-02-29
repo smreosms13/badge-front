515a22960078f2a506169967b365200bebbe3e3b
smreosms13 <smreosms13@gamil.com>
2024-02-29 23:01:57 +0900

    #40 refactor : replace Category as Button and move to CategoryContainer

--- src/components/homepage/certificate/CategoryContainer.js
+++ src/components/homepage/certificate/CategoryContainer.js
@@ -1,16 +1,22 @@
 "use client"
-import Category from "./Category"
-
+import { Button } from "@/components/ui/button";
 export default function CategoryContainer({ contents }) {
     return (
       <div className="flex flex-col mb-2">
         <div className="mb-2">
-          <p className="text-lg">Category</p>
+          <p className="text-lg">카테고리</p>
         </div>
         <div className="grid grid-cols-3 gap-2 mb-2 overflow-x-auto scrollbar-hide">
          {
-          contents.map((categroy, index) => (
-            <Category key={`${categroy.subject}-${index}`} content={categroy}></Category>
+          contents.map((category, index) => (
+            <Button 
+              key={`${category.subject}-${index}`} 
+              variant="outline" 
+              size="sm" 
+              className="rounded-xl text-sm"
+            >
+              {category.subject}
+            </Button>
           ))
          }
         </div>
